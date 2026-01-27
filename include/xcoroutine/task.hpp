d4d7b2d55d1524d1a42105ccb08eb001796c4768
西瓜 <2386104975@qq.com>
2026-01-27 22:17:28 +0800

    feat: 完成task协程类的实现

--- include/xcoroutine/task.hpp
+++ include/xcoroutine/task.hpp
@@ -1,3 +1,227 @@
 #pragma once
+#include <coroutine>
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <sys/stat.h>
+#include <type_traits>
+#include <variant>
+namespace xcoro {
+template <typename T> class task;
 
-namespace xcoro {}
+namespace detail {
+
+class task_promise_base {
+  friend class final_awaiter;
+  struct final_awaiter {
+    bool await_ready() noexcept { return false; }
+    std::coroutine_handle<> await_suspend(std::coroutine_handle<>) noexcept {
+      if (continue_handle_) {
+        return continue_handle_;
+      }
+      return std::noop_coroutine(); // 没有继续的协程 直接返回空句柄，返回主程序
+    }
+    void await_resume() noexcept {} // 返回值被忽略
+    std::coroutine_handle<> continue_handle_;
+  };
+
+public:
+  task_promise_base() = default;
+  ~task_promise_base() = default;
+
+  std::suspend_always initial_suspend() noexcept { return {}; }
+  final_awaiter final_suspend() noexcept { return {continuation_}; }
+
+  void set_continuation(std::coroutine_handle<> continuation) noexcept {
+    continuation_ = continuation;
+  }
+
+protected:
+  std::coroutine_handle<> continuation_;
+};
+
+template <typename T> class task_promise final : public task_promise_base {
+  task_promise(task_promise &&) = delete;
+  task_promise(const task_promise &) = delete;
+  task_promise &operator=(const task_promise &) = delete;
+  task_promise &operator=(task_promise &&) = delete;
+
+  using value_type =
+      std::conditional_t<std::is_reference_v<T>, std::add_pointer_t<T>,
+                         std::remove_const_t<T>>;
+  // remove_const_t 把 tasl<const int> 转成 task<int>
+
+public:
+  task_promise() =
+      default; // 编译器会自动调用promise_type的构造函数，创建promise对象
+  ~task_promise() = default;
+  task<T> get_return_object() noexcept;
+
+  template <typename U>
+    requires std::is_constructible_v<T, U &&>
+  void return_value(U &&value) noexcept {
+    if constexpr (std::is_reference_v<T>) {
+      result_.template emplace<value_type>(std::addressof(value));
+    } else {
+      //   std::cout << "return_value(U &&value) noexcept\n";
+
+      result_.template emplace<T>(std::forward<U>(value));
+    }
+  }
+
+  void unhandled_exception() noexcept {
+    result_.template emplace<std::exception_ptr>(std::current_exception());
+  }
+
+  // 用于场景 auto pro = promise{}; co_await pro;
+  decltype(auto) result() & {
+    if (std::holds_alternative<value_type>(result_)) {
+      if constexpr (std::is_reference_v<T>) {
+        // 通过decltype(auto) + return static_cast<T> 满足返回T&类型和const
+        // T&类型
+        return static_cast<T>(*std::get<value_type>(result_));
+      } else {
+        // 即使T不是引用类型，则返回const T &，去避免拷贝
+        return static_cast<const T &>(std::get<value_type>(result_));
+      }
+    } else if (std::holds_alternative<std::exception_ptr>(result_)) {
+      std::rethrow_exception(std::get<std::exception_ptr>(result_));
+    } else {
+      // 其他情况，例如std::monostate，直接返回
+      throw std::runtime_error("Task result not ready");
+    }
+  }
+
+  // 用于场景 co_await promise{};
+  decltype(auto) result() && {
+    if (std::holds_alternative<value_type>(result_)) {
+      if constexpr (std::is_reference_v<T>) {
+        return static_cast<T>(*std::get<value_type>(result_));
+      } else if constexpr (std::is_move_constructible_v<T>) {
+        // 具有移动构造函数，直接移动构造
+        std::cout << "result() move constructor\n";
+        return std::move(std::get<value_type>(result_));
+      } else {
+        // 没有移动构造
+        // 返回常量右值引用，右值引用也是引用，同样避免拷贝
+        return static_cast<const T &&>(std::get<value_type>(result_));
+      }
+    } else if (std::holds_alternative<std::exception_ptr>(result_)) {
+      std::rethrow_exception(std::get<std::exception_ptr>(result_));
+    } else {
+      throw std::runtime_error("Task result not ready");
+    }
+  }
+
+private:
+  std::variant<std::monostate, value_type, std::exception_ptr> result_;
+};
+
+// 特化void版本
+template <> class task_promise<void> {
+  task_promise(task_promise &&) = delete;
+  task_promise(const task_promise &) = delete;
+  task_promise &operator=(const task_promise &) = delete;
+  task_promise &operator=(task_promise &&) = delete;
+
+public:
+  task<void> get_return_object() noexcept;
+  void return_void() noexcept {}
+  void unhandled_exception() noexcept { exception_ = std::current_exception(); }
+
+  void result() {
+    if (exception_) {
+      std::rethrow_exception(exception_);
+    }
+  }
+
+private:
+  std::exception_ptr exception_;
+};
+
+} // namespace detail
+
+template <typename T> class [[nodiscard]] task {
+public:
+  using promise_type = detail::task_promise<T>;
+  explicit task(std::coroutine_handle<detail::task_promise<T>> handle) {
+    handle_ = handle;
+  }
+  task(const task &) = delete;
+  task &operator=(const task &) = delete;
+
+  task(task &&other) {
+    handle_ = other.handle_;
+    other.handle_ = nullptr;
+  }
+  task &operator=(task &&other) {
+    if (std::addressof(other) == this) {
+      return *this;
+    }
+    if (handle_) {
+      handle_.destroy();
+    }
+    handle_ = other.handle_;
+    other.handle_ = nullptr;
+    return *this;
+  }
+
+  ~task() {
+    if (handle_) {
+      handle_.destroy();
+    }
+  }
+
+  // 用作测试，获取协程句柄
+  std::coroutine_handle<detail::task_promise<T>> handle() const noexcept {
+    return handle_;
+  }
+
+  auto operator co_await() const & noexcept {
+    struct awaitable : public awaitbale_base {
+      decltype(auto) await_resume() {
+        return this->coroutine_.promise().result(); // 保留引用
+      }
+    };
+    return awaitable{handle_};
+  }
+
+  auto operator co_await() const && noexcept {
+    struct awaitable : public awaitbale_base {
+      decltype(auto) await_resume() {
+        // std::cout << "await_resume() move constructor\n";
+        return std::move(this->coroutine_.promise())
+            .result(); // 调用右值版本的result函数
+                       // 返回 T&， const T&&
+      }
+    };
+    return awaitable{handle_};
+  }
+
+  struct awaitbale_base {
+    bool await_ready() noexcept { return false; }
+    std::coroutine_handle<>
+    await_suspend(std::coroutine_handle<> continuation) noexcept {
+      coroutine_.promise().set_continuation(continuation);
+      return coroutine_;
+      // 执行当前协程，保留父协程句柄
+    }
+    std::coroutine_handle<promise_type> coroutine_;
+  };
+
+private:
+  std::coroutine_handle<detail::task_promise<T>> handle_;
+};
+
+template <typename T>
+task<T> detail::task_promise<T>::get_return_object() noexcept {
+  return task<T>(std::coroutine_handle<task_promise<T>>::from_promise(*this));
+}
+
+inline task<void> detail::task_promise<void>::get_return_object() noexcept {
+  return task<void>(
+      std::coroutine_handle<task_promise<void>>::from_promise(*this));
+}
+
+} // namespace xcoro
--- main.cpp
+++ main.cpp
@@ -1,105 +1,25 @@
-#include <coroutine>
-#include <exception>
+#include "xcoroutine/task.hpp"
 #include <iostream>
-class task {
+
+class Data {
 public:
-  struct final_awaiter {
-    bool await_ready() noexcept { return false; }
-    std::coroutine_handle<> await_suspend(std::coroutine_handle<>) noexcept {
-      if (continue_handle_) {
-        return continue_handle_;
-      }
-      return std::noop_coroutine(); // 没有继续的协程 直接返回空句柄，返回主程序
-    }
-    void await_resume() noexcept {} // 返回值被忽略
-
-    std::coroutine_handle<> continue_handle_;
-  };
-
-  struct promise_type {
-    task get_return_object() {
-      return task{std::coroutine_handle<promise_type>::from_promise(*this)};
-    }
-    std::suspend_always initial_suspend() { return {}; }
-    template <typename T> void return_value(T &&value) {
-      std::cout << "return_value: " << value << std::endl;
-    }
-    void unhandled_exception() noexcept {
-      auto exception = std::current_exception();
-      std::rethrow_exception(exception);
-    }
-    auto final_suspend() noexcept { return final_awaiter{continuation_}; }
-
-    std::coroutine_handle<> continuation_;
-  };
-
-  struct awaiter {
-    bool await_ready() noexcept { return false; }
-    std::coroutine_handle<>
-    await_suspend(std::coroutine_handle<> parent) noexcept {
-      handle_.promise().continuation_ = parent;
-      return handle_;
-    }
-    int await_resume() noexcept { return 42; }
-    explicit awaiter(std::coroutine_handle<promise_type> handle)
-        : handle_(handle) {}
-    std::coroutine_handle<promise_type> handle_;
-  };
-
-  awaiter operator co_await() { return awaiter{handle_}; }
-
-  explicit task(std::coroutine_handle<promise_type> handle) : handle_(handle) {}
-
-  ~task() {
-    if (handle_ && handle_.done()) {
-      handle_.destroy();
-    }
-  }
-  void start() {
-    if (handle_) {
-      handle_.resume();
-    }
-  }
-  std::coroutine_handle<> handle() { return handle_; }
-
-private:
-  std::coroutine_handle<promise_type> handle_;
-};
-// final_suspend 必须noexcept 不允许异常破坏协程清理
-// unhandled_exception 必须noexcept 异常处理中不能再抛出异常
-
-struct Awaiter {
-  bool await_ready() noexcept {
-    std::cout << "await_ready" << std::endl;
-    return false;
-  }
-  std::coroutine_handle<>
-  await_suspend(std::coroutine_handle<> handle) noexcept {
-    std::cout << "handle:" << handle.address() << std::endl;
-    std::cout << "await_suspend" << std::endl;
-    return handle;
-  }
-  void await_resume() noexcept { std::cout << "await_resume" << std::endl; }
+  Data() {}
+  Data(const Data &) { std::cout << "Data(const Data &)\n"; }
+  Data(Data &&) { std::cout << "Data(Data &&)\n"; }
 };
 
-task func2() {
-  std::cout << "func2" << std::endl;
-  co_await Awaiter{};
-  std::cout << "func2 after await" << std::endl;
-  co_return "csaniucsa";
+xcoro::task<Data> getName() { co_return Data{}; }
+xcoro::task<int> compute(int x, int y) {
+  Data data = co_await getName();
+  // 有移动构造调用移动构造
+  std::cout << "compute(int x, int y) move constructor\n";
+  // std::cout << str << '\n';
+  co_return x + y;
 }
 
-task func() {
-  std::cout << "func" << std::endl;
-  co_await func2();
-  std::cout << "func after await" << std::endl;
-  co_return "csaniucsa";
+Data &&getData() {
+  Data data;
+  return std::move(data);
 }
 
-int main() {
-  auto coro = func();
-
-  coro.start();
-
-  return 0;
-}
+int main() { Data &&data = getData(); }
